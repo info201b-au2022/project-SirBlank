f53b5bcde0483739f72a3747eaa422630a2a8436
starter821 <56701329+starter821@users.noreply.github.com>
2022-11-15 00:27:11 -0800

    Update pie_chart_metro_b5.R

--- source/pie_chart_metro_b5.R
+++ source/pie_chart_metro_b5.R
@@ -1,12 +1,11 @@
-#Research Question
-# What is the relationshp between health professional shortages and
-# rural hospital shortages? 
 library(tidyverse)
 library(dplyr)
 library(stringr)
 
 library(readr)
-Health_Professional_Shortage_Areas_Primary_Care_by_County_3_ <- read_csv("https://raw.githubusercontent.com/info201b-au2022/project-SirBlank/main/data/Health_Professional_Shortage_Areas__Primary_Care_by_County_3.csv")
+Health_Professional_Shortage_Areas_Primary_Care_by_County_3_ <- read.csv("https://raw.githubusercontent.com/info201b-au2022/project-SirBlank/main/data/Health_Professional_Shortage_Areas__Primary_Care_by_County_3.csv")
+View(Health_Professional_Shortage_Areas_Primary_Care_by_County_3_)
+
 
 metro_nonmetro_hpsap <- Health_Professional_Shortage_Areas_Primary_Care_by_County_3_ %>% 
   group_by(metro_nonmetro)
@@ -19,24 +18,18 @@ metro_hpsap <- metro_nonmetro_hpsap %>%
   filter(metro_nonmetro == "Metropolitan") %>% 
   group_by(value) %>% 
   summarize(num = n()) %>% 
-  mutate("proportion" = num / metro_count$num) %>% 
+  mutate("proportion" = num / metro_count$num * 100) %>% 
   mutate("metro_nonmetro" = "Metropolitan")
 
+pie_metro_data <- data.frame(
+  group = metro_hpsap$value,
+  value = metro_hpsap$proportion
+)
 
-
-# piechart: "Health Professional Shortage Area in Metropolitan"
-pie_metro_data <- c("None of county" = 0.09*100, "Part of county" = 0.438*100, "Whole county" = 0.472*100 )
-pctg_m <- round(pie_metro_data/sum(pie_metro_data)*100, 2)
-label_m <- paste(names(pie_metro_data), pctg_m, "%")
-
-chart_3_metro <- pie(pie_metro_data, clockwise = TRUE, main = "Health Professional Shortage Area in Metropolitan",
-    labels = label_m,
-    radius = 1)
-
-
-
-
-
+chart_3_metro <- ggplot(pie_metro_data, aes(x="", y = value, fill = group)) +
+  geom_bar(stat="identity", width=1, color="white") +
+  coord_polar("y", start = 0) +
+  theme_void()
 
 
 
