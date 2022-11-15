a8dc6289b7c6a8e8ad24655e6c498d5ccb21d69f
teerthatgit <58053621+teerthatgit@users.noreply.github.com>
2022-11-15 10:50:10 -0800

    Update life_expectancy_boxplot_b6.R

--- source/life_expectancy_boxplot_b6.R
+++ source/life_expectancy_boxplot_b6.R
@@ -3,31 +3,27 @@ library(stringr)
 library(ggplot2)
 
 df <- read.csv("https://raw.githubusercontent.com/info201b-au2022/project-SirBlank/main/data/Life_Expectancy_at_Birth.csv")
-View(df)
 
 df_2 <- read.csv("https://raw.githubusercontent.com/info201b-au2022/project-SirBlank/main/data/Health_Professional_Shortage_Areas__Primary_Care_by_County_3.csv")
-View(df_2)
 
 df_3 <- read.csv("https://raw.githubusercontent.com/info201b-au2022/project-SirBlank/main/data/state_to_abbreviation.csv")
-View(df_3)
 
 df_4 <- left_join(df_2, df_3, by = c("state_abbr" = "code"))
-View(df_4)
 
 df_4 <- mutate(
   df_4,
   county_state = paste(county_name,"(",state,")"),
   )
-View(df_4)
+
 
 final_df <- full_join(df, df_4, by = c("location_name" = "state" ))
-View(final_df)
+
 
 final_df <- na.omit(final_df)
 final_df <- subset(final_df, select = -c(county_state))
 
 final_final_df <- filter(final_df, age_name == "<1 year")
-View(final_final_df)
+
 
 metro <- final_final_df%>%
   filter(metro_nonmetro == "Metropolitan")%>%
