4abeae9870a1721e6d4c513d00bb083b55641af1
starter821 <56701329+starter821@users.noreply.github.com>
2022-12-07 20:14:27 -0800

    Add files via upload

--- source/pie_chart_metro_b5.R
+++ source/pie_chart_metro_b5.R
@@ -1,8 +1,10 @@
 library(tidyverse)
 library(dplyr)
 library(stringr)
+library(plotly)
 
 library(readr)
+
 Health_Professional_Shortage_Areas_Primary_Care_by_County_3_ <- read.csv("https://raw.githubusercontent.com/info201b-au2022/project-SirBlank/main/data/Health_Professional_Shortage_Areas__Primary_Care_by_County_3.csv")
 
 
@@ -24,7 +26,8 @@ metro_hpsap <- metro_nonmetro_hpsap %>%
 pie_metro_data <- data.frame(
   group = metro_hpsap$value,
   value = metro_hpsap$proportion,
-  labels = paste(round(metro_hpsap$proportion, 2), "%")
+  labels = paste(round(metro_hpsap$proportion, 2), "%"),
+  metro_nonmetro = nonmetro_hpsap$metro_nonmetro
 )
 
 chart_3_metro <- ggplot(pie_metro_data, aes(x="", y = value, fill = group)) +
@@ -33,6 +36,17 @@ chart_3_metro <- ggplot(pie_metro_data, aes(x="", y = value, fill = group)) +
   ggtitle("Health Professional Shortage Area in Metropolitan") +
   geom_text(aes(label = labels), position = position_stack(vjust = 0.5)) +
   theme_void()
+
+chart_3_metro <- plot_ly(pie_metro_data,
+                         labels = ~group,
+                         values = ~value,
+                         type = 'pie')
+
+chart_3_metro <- chart_3_metro %>% 
+  layout(title = 'Health Professional Shortage Area in Metropolitan') %>% 
+  add_trace(hovertemplate = "%{label}: <br>Percentage: %{percent}")
+
+chart_3_metro
 
 
 
